4253e899ea4b094c0d4defc1637a8659937a5187
Thársos Lima Gonçalves Xavier Souza <art0072699@aluno.firjansenaisesi.com.br>
2026-09-17 16:38:10 -0300

    create exercicio-veiculos

---
+++ exercicio-veiculos/Principal.js
@@ -0,0 +1,8 @@
+alert("Hello World!")
+console.log("Hello World!")
+
+import { calculo } from "calculos.js";
+import { Caminhao } from "Caminhao.js";
+import { Carro } from "Carro.js";
+import { Moto } from "Moto.js";
+import { Veiculo } from "Veiculo.js";
