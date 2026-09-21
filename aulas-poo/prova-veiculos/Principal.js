a2339f8e5ab8de8fec846cb55cac7ddcfd6775d4
Thársos Lima Gonçalves Xavier Souza <art0072699@aluno.firjansenaisesi.com.br>
2026-09-21 16:00:51 -0300

    import das classes

---
+++ aulas-poo/prova-veiculos/Principal.js
@@ -0,0 +1,10 @@
+import { Veiculo } from "./Veiculo.js";
+
+import { VeiculoTerrestre } from "./VeiculoTerrestre.js";
+import { Carro } from "./Carro.js"; 
+import { Moto } from "./Moto.js";
+
+import { VeiculoAquatico } from "./VeiculoAquatico.js";
+import { Barco } from "./Barco.js";
+import { Jetski } from "./Jetski.js";
+
